9f62a4ac9a374c28763a0b565a38840ac5cae23d
rahmat <rahmat3nanda@nardoo.com.au>
2019-11-20 11:31:30 +0700

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    title: "Card & Parsing",
+    home: new HalSatu(),
+  ));
+}
+
+class HalSatu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Card & Parsing"),
+      ),
+      body: new Container(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            new CardSaya(
+              icon: Icons.home,
+              teks: "Home",
+              warnaIcon: Colors.brown,
+            ),
+            new CardSaya(
+              icon: Icons.favorite,
+              teks: "favorite",
+              warnaIcon: Colors.pink,
+            ),
+            new CardSaya(
+              icon: Icons.place,
+              teks: "place",
+              warnaIcon: Colors.blue,
+            ),
+            new CardSaya(
+              icon: Icons.settings,
+              teks: "Settings",
+              warnaIcon: Colors.black,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardSaya extends StatelessWidget {
+  CardSaya({this.icon, this.teks, this.warnaIcon});
+  final IconData icon;
+  final String teks;
+  final Color warnaIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: new EdgeInsets.all(10.0),
+      child: new Card(
+          child: new Column(
+            children: <Widget>[
+              new Icon(icon, size: 50.0, color: warnaIcon),
+              new Text(
+                teks,
+                style: new TextStyle(fontSize: 20.0),
+              ),
+            ],
+          )),
+    );
+  }
+}
